7729ecdc677b282d46f066b2013a3ab75bcbf450
Dear User <pouy>
2026-07-24 13:58:38 +0330

    add *

---
+++ main_02.py
@@ -0,0 +1,58 @@
+import pgzrun
+import random
+from pgzero.actor import Actor 
+from pgzero.keyboard import keyboard
+
+def draw():
+    screen.blit("back", (0, 0))
+    mario.draw()
+    luigi.draw()
+    enemy.draw()
+    coin.draw()
+
+def update():
+    if keyboard.right:
+        luigi.x += 5
+        luigi.image = "luigi_right"
+    if keyboard.left:
+        luigi.x -= 5
+        luigi.image = "luigi_left"
+    if keyboard.down:
+        luigi.y += 5
+    if keyboard.up:
+        luigi.y -= 5
+    
+    if keyboard.d:
+        mario.x += 5
+        mario.image = "mario_right"
+    if keyboard.a:
+        mario.x -= 5
+        mario.image = "mario_left"
+    if keyboard.s:
+        mario.y += 5
+    if keyboard.w:
+       mario.y -= 5
+
+WIDTH = 1280
+HEIGHT = 720
+
+luigi = Actor("luigi_right")
+luigi .x = 600
+luigi .y = 200
+
+mario = Actor("mario_right")
+mario.x = random.randint(0, 1280)
+mario.x = random.randint(0, 720)
+mario.x = 300
+mario.y = 200
+
+
+enemy = Actor("enemy_right")
+enemy.x = 700
+enemy.y = 300
+
+coin = Actor("coin")
+coin.x = 700
+coin.y = 400
+
+pgzrun.go()
